4d961c4b6ed89090d83d2c701583aeda4b2b4205
Isabella <belladougherty@gmail.com>
2023-03-07 09:14:23 -0700

    Added an easter egg where when the fish button is hit 10 times a blob fish appears and when any other button is clicked it dissapears again as well as added comments

--- DinerMenu/DinerMenuForm.Designer.vb
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -26,7 +26,10 @@ Partial Class Form1
         Me.SoupButton = New System.Windows.Forms.Button()
         Me.SaladButton = New System.Windows.Forms.Button()
         Me.FishButton = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DailySpecialLabel = New System.Windows.Forms.Label()
+        Me.BlobFishPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.BlobFishPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DinerNameLabel
@@ -46,7 +49,7 @@ Partial Class Form1
         Me.SoupButton.Font = New System.Drawing.Font("Constantia", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.SoupButton.Location = New System.Drawing.Point(12, 368)
         Me.SoupButton.Name = "SoupButton"
-        Me.SoupButton.Size = New System.Drawing.Size(181, 67)
+        Me.SoupButton.Size = New System.Drawing.Size(249, 67)
         Me.SoupButton.TabIndex = 1
         Me.SoupButton.Text = "Soup"
         Me.SoupButton.UseVisualStyleBackColor = False
@@ -55,9 +58,9 @@ Partial Class Form1
         '
         Me.SaladButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.SaladButton.Font = New System.Drawing.Font("Constantia", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.SaladButton.Location = New System.Drawing.Point(204, 371)
+        Me.SaladButton.Location = New System.Drawing.Point(267, 368)
         Me.SaladButton.Name = "SaladButton"
-        Me.SaladButton.Size = New System.Drawing.Size(182, 67)
+        Me.SaladButton.Size = New System.Drawing.Size(265, 67)
         Me.SaladButton.TabIndex = 2
         Me.SaladButton.Text = "Salad"
         Me.SaladButton.UseVisualStyleBackColor = False
@@ -66,36 +69,57 @@ Partial Class Form1
         '
         Me.FishButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.FishButton.Font = New System.Drawing.Font("Constantia", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.FishButton.Location = New System.Drawing.Point(392, 371)
+        Me.FishButton.Location = New System.Drawing.Point(538, 368)
         Me.FishButton.Name = "FishButton"
-        Me.FishButton.Size = New System.Drawing.Size(181, 67)
+        Me.FishButton.Size = New System.Drawing.Size(250, 67)
         Me.FishButton.TabIndex = 3
         Me.FishButton.Text = "Fish"
         Me.FishButton.UseVisualStyleBackColor = False
         '
-        'Button1
+        'ExitButton
         '
-        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.Button1.Font = New System.Drawing.Font("Constantia", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Button1.Location = New System.Drawing.Point(579, 371)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(209, 67)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Daily Special"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.ExitButton.Location = New System.Drawing.Point(741, 9)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(47, 23)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DailySpecialLabel
+        '
+        Me.DailySpecialLabel.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point)
+        Me.DailySpecialLabel.Location = New System.Drawing.Point(12, 87)
+        Me.DailySpecialLabel.Name = "DailySpecialLabel"
+        Me.DailySpecialLabel.Size = New System.Drawing.Size(776, 278)
+        Me.DailySpecialLabel.TabIndex = 6
+        Me.DailySpecialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BlobFishPictureBox
+        '
+        Me.BlobFishPictureBox.Image = Global.DinerMenu.My.Resources.Resources.Blobfish_feels_pretty
+        Me.BlobFishPictureBox.Location = New System.Drawing.Point(222, 90)
+        Me.BlobFishPictureBox.Name = "BlobFishPictureBox"
+        Me.BlobFishPictureBox.Size = New System.Drawing.Size(373, 275)
+        Me.BlobFishPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.BlobFishPictureBox.TabIndex = 7
+        Me.BlobFishPictureBox.TabStop = False
+        Me.BlobFishPictureBox.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.BlobFishPictureBox)
+        Me.Controls.Add(Me.DailySpecialLabel)
+        Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.FishButton)
         Me.Controls.Add(Me.SaladButton)
         Me.Controls.Add(Me.SoupButton)
         Me.Controls.Add(Me.DinerNameLabel)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.BlobFishPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -105,5 +129,7 @@ Partial Class Form1
     Friend WithEvents SoupButton As Button
     Friend WithEvents SaladButton As Button
     Friend WithEvents FishButton As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DailySpecialLabel As Label
+    Friend WithEvents BlobFishPictureBox As PictureBox
 End Class
--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -1,3 +1,51 @@
 ﻿Public Class Form1
+    Dim dailySpecial As String
+    Dim blob As Integer
 
+    'Uses the dailySpecial variable to assign a string describing the daily soup special. Then assigns the DailySpecialLabel to that string
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        dailySpecial = "- Lobster Bisque - 
+Our rich and creamy bisque is made with fresh, succulent 
+lobster meat and slow-cooked to perfection with aromatic vegetables, 
+herbs, and spices. Served with a dollop of crème fraîche and a sprinkle 
+of chopped chives, this classic French soup is the ultimate indulgence. 
+Perfect as a starter or a light meal on its own."
+        DailySpecialLabel.Text = dailySpecial
+        blob = 0
+        BlobFishPictureBox.Visible = False
+    End Sub
+
+    'Uses the dailySpecial variable to assign a string describing the daily salad special. Then assigns the DailySpecialLabel to that string
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        dailySpecial = "- Caprese Salad - 
+A light and refreshing salad made with vine-ripe tomatoes, 
+creamy fresh mozzarella, and fragrant basil leaves. Drizzled 
+with extra-virgin olive oil and a balsamic glaze, this classic 
+Italian dish is a perfect appetizer or light lunch."
+        DailySpecialLabel.Text = dailySpecial
+        blob = 0
+        BlobFishPictureBox.Visible = False
+    End Sub
+
+    'Uses the dailySpecial variable to assign a string describing the daily fish special. Then assigns the DailySpecialLabel to that string
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        dailySpecial = "- Linguine alle Vongole - 
+Our classic pasta dish features linguine tossed with fresh clams, garlic, 
+white wine, and parsley, all sautéed to perfection. Served with a sprinkle of 
+red pepper flakes for a touch of heat and a squeeze of lemon juice, this simple 
+and flavorful dish is a crowd-pleaser. Perfect for seafood lovers or anyone looking 
+for a taste of Italy."
+        DailySpecialLabel.Text = dailySpecial
+        blob += 1
+        If blob >= 10 Then
+            BlobFishPictureBox.Visible = True
+        Else
+            BlobFishPictureBox.Visible = False
+        End If
+    End Sub
+
+    'Ends the program
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        End
+    End Sub
 End Class
